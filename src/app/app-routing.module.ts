76398500d3f81036302c0e30932fe85da311e84b
Msaed Sahar <sahar.msaed@inetum.com>
2022-11-07 16:42:04 +0100

    ajout routes et router-outlet

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PageListOrdersComponent } from './orders/pages/page-list-orders/page-list-orders.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'orders', component: PageListOrdersComponent  },
+  { path: 'sahar', redirectTo: 'orders', pathMatch: 'full'  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
