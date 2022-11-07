77a9c10821b57a9ca8467937593c871a9f50cc3a
Msaed Sahar <sahar.msaed@inetum.com>
2022-11-07 14:18:22 +0100

    ajout bootstrap

---
+++ src/app/orders/pages/page-add-order/page-add-order.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-add-order',
+  templateUrl: './page-add-order.component.html',
+  styleUrls: ['./page-add-order.component.css']
+})
+export class PageAddOrderComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
---
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-page-list-orders',
+  templateUrl: './page-list-orders.component.html',
+  styleUrls: ['./page-list-orders.component.css']
+})
+export class PageListOrdersComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
