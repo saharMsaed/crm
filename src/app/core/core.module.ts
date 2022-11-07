493a1e9dd2be253fc7f2465f45fb562e18802fe2
Msaed Sahar <sahar.msaed@inetum.com>
2022-11-07 13:52:56 +0100

    import core module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { OrdersModule } from '../orders/orders.module';
 
 
 
@@ -7,6 +8,9 @@ import { CommonModule } from '@angular/common';
   declarations: [],
   imports: [
     CommonModule
+  ],
+  exports: [
+    OrdersModule
   ]
 })
 export class CoreModule { }
