12bc804b7d015aaef4bd4bba781b6037410a2963
Msaed Sahar <sahar.msaed@inetum.com>
2022-11-08 09:13:23 +0100

    pre loading

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,5 +1,5 @@
 import { NgModule } from '@angular/core';
-import { RouterModule, Routes } from '@angular/router';
+import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 import { PageListOrdersComponent } from './orders/pages/page-list-orders/page-list-orders.component';
 
 const routes: Routes = [
@@ -12,7 +12,7 @@ const routes: Routes = [
 ];
 
 @NgModule({
-  imports: [RouterModule.forRoot(routes)],
+  imports: [RouterModule.forRoot(routes, { preloadingStrategy: PreloadAllModules })],
   exports: [RouterModule]
 })
 export class AppRoutingModule { }
