b28a2ee4ae2be4999fb171de3ffc474e93ca87e9
Msaed Sahar <sahar.msaed@inetum.com>
2022-11-08 11:46:00 +0100

    var env apiUrl

--- src/app/orders/services/orders.service.ts
+++ src/app/orders/services/orders.service.ts
@@ -2,18 +2,21 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { Order } from 'src/app/core/models/order';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class OrdersService {
   public collection$: Observable<Order[]>;
+  private urlApi: string;
 
   constructor(private httpClient: HttpClient) {
+    this.urlApi = `${environment.urlApi}`;
+    // this.urlApi = environment.urlApi + '/orders';
     // init flux de données
-    this.collection$ = this.httpClient.get<Order[]>(
-      '/orders'
-    );
+    this.collection$ = this.httpClient.get<Order[]>(`${this.urlApi}/orders`);
     console.log(this.collection$);
   }
+
 }
