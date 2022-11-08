cadc89af1a3e13a4fca80eca0902e6557dee49cd
Msaed Sahar <sahar.msaed@inetum.com>
2022-11-08 13:57:59 +0100

    affichage collection

--- src/app/orders/pages/page-list-orders/page-list-orders.component.ts
+++ src/app/orders/pages/page-list-orders/page-list-orders.component.ts
@@ -9,8 +9,10 @@ import { OrdersService } from '../../services/orders.service';
 })
 export class PageListOrdersComponent implements OnInit {
   public collection!: Order[];
+  public headers!: string[];
 
   constructor(private ordersService: OrdersService) {
+    this.headers = ['Type', 'Client', 'Durée', 'Tjm HT', 'Total HT', 'Total TTC', 'Statut']
     // souscrire pour récupérer dans le callback les données
     this.ordersService.collection$.subscribe((data) => {
       this.collection = data;
